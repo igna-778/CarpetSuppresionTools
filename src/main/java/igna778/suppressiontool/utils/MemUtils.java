d7fdf4b2101a548081818896287cd5f89e690a6e
igna778 <ignacio@castanleiva.es>
2024-10-14 12:04:27 +0200

    fix: added necessary functions for OOMBlock code

--- src/main/java/igna778/suppressiontool/utils/MemUtils.java
+++ src/main/java/igna778/suppressiontool/utils/MemUtils.java
@@ -1,7 +1,11 @@
 package igna778.suppressiontool.utils;
 
 public class MemUtils {
-    private static final double f = 0.75;
+    private static final int sizeOfBuff = 1024;
+    private static byte[][] membuff = new byte[sizeOfBuff][];
+    private static final int bchunkSize = 5 * 1024 * 1024; // 5 MB
+
+    private static final double f = 0.75; // Const for upsize calculation
 
     public static int powerOfTwo(long x){
         return 64 - Long.numberOfLeadingZeros(x - 1);
@@ -21,4 +25,36 @@ public class MemUtils {
         return (long) Math.ceil(expected * f)+1; // Maxfill * 0.75
     }
 
+    public static void simulateOOM(int bSize) {
+        if(bSize <= 0)
+            bSize = bchunkSize;
+        int i = 0;
+        try {
+            while (true){
+                if(i > membuff.length){
+                    throw new OutOfMemoryError("Artificial Out Of Memory due to Array Index out of bounds");
+                }
+                membuff[i] = new byte[bSize]; // Use 10MB of memory
+                i++;
+            }
+        } catch (OutOfMemoryError err) {
+            membuff = null;
+            membuff = new byte[sizeOfBuff][];
+            throw err;
+        }
+    }
+
+    public static long getFreeMemory() {
+        try {
+            Runtime rntime = Runtime.getRuntime();
+            long maxmem = rntime.maxMemory();
+            long freemem = rntime.freeMemory();
+            long totalmem = rntime.totalMemory();
+
+            //The memory that we haven't allocated yet (Max - current total) plus the free amount from the total (+ Free)
+            return maxmem - totalmem + freemem;
+        } catch (OutOfMemoryError e) {
+            throw  new OutOfMemoryError("OutOfMemory while calculating free memory");
+        }
+    }
 }
